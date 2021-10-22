990d970a8d4ae99d759a2ee732ff4b463667e92a
Mahesh <mahesh@kali.kali>
2021-10-22 08:32:26 -0700

    javascript Update

---
+++ mani.js
@@ -0,0 +1,14 @@
+console.log("mahesh1706");
+
+let name = "mahesh$kali"
+let msg = "I There.. My Name Is " + name +  "      I'm Learning kali linux For Hacking"
+
+console.log(name);
+//alert(name);//
+console.log(msg);
+
+let number1 = 5;
+let number2 = 60;
+
+let result = number1 + number2 ;
+console.log(result);
